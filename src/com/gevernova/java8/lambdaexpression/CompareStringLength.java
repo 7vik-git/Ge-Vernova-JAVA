5d225d09bd530e8c8c0c5339b1a482033ff1bbb1
7vik-git <sathviklingamsetti@gmail.com>
2025-05-19 14:04:38 +0530

    Added New Files

--- src/com/gevernova/java8/lambdaexpression/CompareStringLength.java
+++ src/com/gevernova/java8/lambdaexpression/CompareStringLength.java
@@ -7,7 +7,7 @@ import java.util.List;
 public class CompareStringLength {
     public static void main(String[] args) {
         List<String> names = Arrays.asList("Hello","World","jon","wick","wdjdcefv","wqdjdnvieqrv","wef");
-        names.sort((s1,s2) -> Integer.compare(s2.length(),s1.length()));
+        names.sort((s1,s2) -> Integer.compare(s2.length(),s1.length()))  ;
         System.out.println(names);
     }
 }
